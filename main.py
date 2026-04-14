f4ebd017c3d9b09b81b9785f2c355a532884f540
lavan <lsindhu@calpoly.edu>
2026-04-14 09:58:48 -0700

    first commit

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 # Create a welcome message.
 # Input: a name as a string
 # Result: a string
-def welcome_message(a:str) -> str:
-   message = "Hello, " + a + "."
+def welcome_message(name:str) -> str:
+   message = "Hello, " + name + "."
 
    return message
 
